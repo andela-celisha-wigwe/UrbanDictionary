754c185ca62c46b282315a4287728855056232f8
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-02-10 15:06:12 +0100

    fixing test bug

--- tests/CategoryTest.php
+++ tests/CategoryTest.php
@@ -6,59 +6,12 @@
 // use Elchroy\UrbanDictionary\WordEngine;
 
 // // use \Elchroy\UrbanDictionary\Category;
-// // use Elchroy\Tests\Category;
+use Elchroy\UrbanDictionary\Category;
 // // use Elchroy\UrbanDictionary\WordEngine;
 // //
 // include 'Category.php';
 
 
-
-class Category extends \Elchroy\UrbanDictionary\WordEngine
-{
-    public $main;
-
-    public function __construct()
-    {
-        $this->main = \Elchroy\UrbanDictionary\Word::$data;
-    }
-
-    public function categorize($slang, $property = 'sample_sentence')
-    {
-        $sentence = $this->retrieve($slang, $property);
-        return $this->groupOf($sentence);
-    }
-
-    public function categorize_i($slang, $property = 'sample_sentence')
-    {
-        $sentence = $this->retrieve($slang, $property);
-        $sentence = strtolower($sentence);
-        return $this->groupOf($sentence);
-    }
-
-    public function groupOf($sentence)
-    {
-        $sentence = trim($sentence); //trim the sentence of all leading and trailing white spaces
-        $sentence = preg_replace("/[^a-zA-Z0-9\s]/", "", $sentence); // remove all non-alphanumercic charaters.
-        $count_array = array();
-        $words = explode(" ", $sentence);
-        foreach ($words as $word) {
-            $count = $this->getCount($words, $word);
-            $count_array[$word] = $count;
-        }
-        return $count_array;
-    }
-
-    private function getCount($words, $word)
-    {
-        $count = 0;
-        foreach($words as $second_word){
-            if($word == $second_word){
-                $count++;
-            }
-        }
-        return $count;
-    }
-}
 
 
 
