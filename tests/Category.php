d79906fd2066dba770e34b4942ecf4850f7da85f
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-02-10 14:46:18 +0100

    Deactivated namespaces to try test.

---
+++ tests/Category.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Elchroy\Test;
+
+use Elchroy\UrbanDictionary\Word;
+use Elchroy\UrbanDictionary\WordEngine;
+
+class Category extends WordEngine
+{
+    public $main;
+
+    public function __construct()
+    {
+        $this->main = Word::$data;
+    }
+
+    public function categorize($slang, $property = 'sample_sentence')
+    {
+        $sentence = $this->retrieve($slang, $property);
+        return $this->groupOf($sentence);
+    }
+
+    public function categorize_i($slang, $property = 'sample_sentence')
+    {
+        $sentence = $this->retrieve($slang, $property);
+        $sentence = strtolower($sentence);
+        return $this->groupOf($sentence);
+    }
+
+    public function groupOf($sentence)
+    {
+        $sentence = trim($sentence); //trim the sentence of all leading and trailing white spaces
+        $sentence = preg_replace("/[^a-zA-Z0-9\s]/", "", $sentence); // remove all non-alphanumercic charaters.
+        $count_array = array();
+        $words = explode(" ", $sentence);
+        foreach ($words as $word) {
+            $count = $this->getCount($words, $word);
+            $count_array[$word] = $count;
+        }
+        return $count_array;
+    }
+
+    private function getCount($words, $word)
+    {
+        $count = 0;
+        foreach($words as $second_word){
+            if($word == $second_word){
+                $count++;
+            }
+        }
+        return $count;
+    }
+}
+
--- tests/CategoryTest.php
+++ tests/CategoryTest.php
@@ -1,14 +1,69 @@
 <?php
 
-namespace Elchroy\Tests;
+// // namespace Elchroy\Tests;
 
-// use Elchroy\UrbanDictionary\Word;
-// use Elchroy\UrbanDictionary\WordEngine;
+use Elchroy\UrbanDictionary\Word;
+use Elchroy\UrbanDictionary\WordEngine;
 
-use \Elchroy\UrbanDictionary\Category;
+// // use \Elchroy\UrbanDictionary\Category;
+// // use Elchroy\Tests\Category;
+// // use Elchroy\UrbanDictionary\WordEngine;
+// //
+// include 'Category.php';
 
 
-class CategoryTest extends \PHPUnit_Framework_TestCase
+
+class Category extends WordEngine
+{
+    public $main;
+
+    public function __construct()
+    {
+        $this->main = Word::$data;
+    }
+
+    public function categorize($slang, $property = 'sample_sentence')
+    {
+        $sentence = $this->retrieve($slang, $property);
+        return $this->groupOf($sentence);
+    }
+
+    public function categorize_i($slang, $property = 'sample_sentence')
+    {
+        $sentence = $this->retrieve($slang, $property);
+        $sentence = strtolower($sentence);
+        return $this->groupOf($sentence);
+    }
+
+    public function groupOf($sentence)
+    {
+        $sentence = trim($sentence); //trim the sentence of all leading and trailing white spaces
+        $sentence = preg_replace("/[^a-zA-Z0-9\s]/", "", $sentence); // remove all non-alphanumercic charaters.
+        $count_array = array();
+        $words = explode(" ", $sentence);
+        foreach ($words as $word) {
+            $count = $this->getCount($words, $word);
+            $count_array[$word] = $count;
+        }
+        return $count_array;
+    }
+
+    private function getCount($words, $word)
+    {
+        $count = 0;
+        foreach($words as $second_word){
+            if($word == $second_word){
+                $count++;
+            }
+        }
+        return $count;
+    }
+}
+
+
+
+
+class CategoryTest extends PHPUnit_Framework_TestCase
 {
     public $cat;
     public function setUp()
