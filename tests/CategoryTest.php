3231ba9d79f319b46fc8830d44359f32c5058b9a
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-02-10 12:41:56 +0100

    Modified CategoyTest.php

--- tests/CategoryTest.php
+++ tests/CategoryTest.php
@@ -1,11 +1,14 @@
 <?php
 
-use Elchroy\UrbanDictionary\Word;
-use Elchroy\UrbanDictionary\WordEngine;
+namespace Elchroy\Tests;
+
+// use Elchroy\UrbanDictionary\Word;
+// use Elchroy\UrbanDictionary\WordEngine;
+
 use Elchroy\UrbanDictionary\Category;
 
 
-class CategoryTest extends PHPUnit_Framework_TestCase
+class CategoryTest extends \PHPUnit_Framework_TestCase
 {
     public $cat;
     public function setUp()
