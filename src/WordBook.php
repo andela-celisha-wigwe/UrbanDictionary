23906743ae3da02a71d1b59e63239f50e3aa134e
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2016-02-16 23:06:20 +0100

    edited WordBook.php

--- src/WordBook.php
+++ src/WordBook.php
@@ -111,7 +111,7 @@ class WordBook
         //get eh length og the string. and chhose only those words that start with the letters given in the argument.
         $result = (array_filter(array_keys($this->main), function($slang) use ($string) {return $slang !== $string;}));
         $result = array_map(function ($key) {return $this->main[$key];} , $result);
-        var_dump(reset($result));
+        return $result;
         // return array_filter($this->main, function($slang) use ($string) {$slang == $string;});
     }
 
